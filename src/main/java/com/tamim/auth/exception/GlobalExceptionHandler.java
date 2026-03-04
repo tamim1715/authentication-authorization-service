e2c02f5ee7c9600de25e137ac92e5531ce74fe6f
tamim <shahadathhossain447@gmail.com>
2026-03-04 23:05:40 +0600

    Log added in exception

--- src/main/java/com/tamim/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tamim/auth/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.tamim.auth.exception;
 
 import com.tamim.auth.dto.error.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.Instant;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiErrorResponse> handleApiException(ApiException ex) {
+
+        log.warn("Business error: {}", ex.getMessage());
+
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(
@@ -27,6 +32,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleUnexpected(Exception ex) {
+
+        log.error("Unexpected system error", ex);
+
         return ResponseEntity
                 .internalServerError()
                 .body(
